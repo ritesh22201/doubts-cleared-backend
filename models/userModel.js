758d066d84cea04714e35ce904916537747c7dfc
ritesh22201 <riteshgoswami22201@gmail.com>
2023-11-12 15:06:25 +0530

    class added

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
     password : {type : String, required : true},
     userType : {type : String, enum : ['student', 'tutor'], required : true},
     isVerified : {type : Boolean, default : false},
+    class : {type : String, required : false},
     otp : String
 })
 
