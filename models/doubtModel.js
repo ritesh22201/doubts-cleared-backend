53dbb21e3a81cae99d61004d4b7ba243d3708fdf
ritesh22201 <riteshgoswami22201@gmail.com>
2023-11-13 13:21:33 +0530

    subjects added

--- models/doubtModel.js
+++ models/doubtModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const doubtSchema = new mongoose.Schema({
     studentId : {type : mongoose.Schema.Types.ObjectId, ref : 'user', required : true},
-    subject : {type : String, required : true},
+    subject : {type : String, enum : ['mathematics', 'chemistry', 'biology', 'computer science', 'history', 'geography', 'english'], required : true},
     question : {type : String, required : true},
     isResolved : {type : Boolean, default : false},
     timestamp : {type : Date, default : Date.now}
