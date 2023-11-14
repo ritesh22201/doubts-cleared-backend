f9149359fda3acfb511687dc77ff79cdae4ae9c2
ritesh22201 <riteshgoswami22201@gmail.com>
2023-11-14 07:31:07 +0530

    user route corrected

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    name : String,
+    avatar : String,
     email : {type : String, required : true, unique : true},
     password : {type : String, required : true},
     userType : {type : String, enum : ['student', 'tutor'], default : 'student'},
